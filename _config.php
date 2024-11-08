d4f4f5697217afb8814bb401a7a50b3dd5e8d2e5
Davi-barros-tech <davibarros_2017@hotmail.com>
2024-11-08 17:07:44 -0400

    envio-1

---
+++ _config.php
@@ -0,0 +1,12 @@
+﻿<?php
+$config = array();
+$config['SERVIDOR'] = "localhost";
+$config['BANCO'] = "";
+$config['USUARIO'] = "";
+$config['SENHA'] = "";
+$config['PASTA'] = "";/* Caso seu site não seja instalo na raiz você preenche o campo com o nome da pasta*/
+$config['SSL'] = true; /*Digite false caso não possua certificado digital*/
+$config['token1'] = "digitequalquercoisa";
+$config['token2'] = "digitequalquercoisa";
+$config['recaptcha_key'] = "";
+$config['recaptcha_secret'] = "";
